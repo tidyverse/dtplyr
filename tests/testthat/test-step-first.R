18930c58f0c90b0c4858ffbf654df433a78e0114
Hadley Wickham <h.wickham@gmail.com>
2019-06-24 17:56:35 -0500

    Test step_first

---
+++ tests/testthat/test-step-first.R
@@ -0,0 +1,22 @@
+test_that("constructor has sensible defaults", {
+  dt <- data.table(x = 1:2, y = 1:2)
+  step <- new_step_first(dt)
+
+  expect_s3_class(step, "dtplyr_step_first")
+  expect_equal(step$parent, dt)
+  expect_equal(step$vars, c("x", "y"))
+  expect_equal(step$groups, character())
+})
+
+test_that("doesn't need copy", {
+  dt <- lazy_dt(mtcars)
+  expect_false(dt_needs_copy(dt))
+})
+
+test_that("dt_call() copies if requested", {
+  dt <- lazy_dt(mtcars)
+
+  expect_equal(dt_call(dt, FALSE), quote(`_DT`))
+  expect_equal(dt_call(dt, TRUE), quote(copy(`_DT`)))
+})
+
