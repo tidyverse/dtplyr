97ea97007750c9192c195e4085050d5b3cad076f
Hadley Wickham <h.wickham@gmail.com>
2019-06-27 07:41:11 -0500

    Complete test coverage for step class

--- tests/testthat/test-step.R
+++ tests/testthat/test-step.R
@@ -1,4 +1,12 @@
-test_that("group metadata", {
+test_that("tbl metadata as expected", {
+  dt <- lazy_dt(data.table(x = c(1, 1, 1, 2, 2, 3)), "DT")
+
+  expect_equal(dim(dt), c(NA, 1))
+  expect_equal(tbl_vars(dt), "x")
+  expect_equal(show_query(dt), expr(DT))
+})
+
+test_that("group metadata as expected", {
   dt <- lazy_dt(data.table(x = c(1, 1, 1, 2, 2, 3)))
   expect_equal(group_vars(dt), character())
   expect_equal(groups(dt), list())
@@ -10,4 +18,19 @@ test_that("group metadata", {
   expect_equal(groups(gt), syms("x"))
   expect_equal(group_size(gt), c(3, 2, 1))
   expect_equal(n_groups(gt), 3)
+})
+
+test_that("has useful print method", {
+  dt <- lazy_dt(mtcars, "DT")
+  expect_known_output(print(dt), test_path("test-step-print.txt"))
+})
+
+test_that("can evaluate to any data frame type", {
+  dt <- lazy_dt(mtcars, "DT")
+
+  expect_identical(class(as.data.frame(dt)), "data.frame")
+  expect_s3_class(as.data.table(dt), "data.table")
+  expect_s3_class(as_tibble(dt), "tbl_df")
+
+  expect_s3_class(collect(dt), "data.table")
 })
