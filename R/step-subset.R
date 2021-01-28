60f0f11515d854a1d12e92623ca18cd951d3cf9c
Hadley Wickham <h.wickham@gmail.com>
2021-01-28 17:17:12 -0600

    Use newer eval_select() approach

--- R/step-subset.R
+++ R/step-subset.R
@@ -163,9 +163,8 @@ ensure_group_vars <- function(loc, names, groups) {
 #' @importFrom dplyr relocate
 #' @export
 relocate.dtplyr_step <- function(.data, ..., .before = NULL, .after = NULL) {
-  vars <- .data$vars
-  to_move <- match(tidyselect::vars_select(vars, ...), vars)
-  groups <- .data$groups
+  sim_data <- simulate_vars(.data)
+  to_move <- tidyselect::eval_select(expr(c(...)), sim_data)
 
   if (length(to_move) == 0) {
     return(.data)
@@ -179,12 +178,12 @@ relocate.dtplyr_step <- function(.data, ..., .before = NULL, .after = NULL) {
   if (has_before && has_after) {
     abort("Must supply only one of `.before` and `.after`.")
   } else if (has_before) {
-    where <- min(match(tidyselect::vars_select(vars, !!.before), vars))
+    where <- min(unname(tidyselect::eval_select(.before, sim_data)))
     if (!where %in% to_move) {
       to_move <- c(to_move, where)
     }
   } else if (has_after) {
-    where <- max(match(tidyselect::vars_select(vars, !!.after), vars))
+    where <- max(unname(tidyselect::eval_select(.after, sim_data)))
     if (!where %in% to_move) {
       to_move <- c(where, to_move)
     }
@@ -197,10 +196,10 @@ relocate.dtplyr_step <- function(.data, ..., .before = NULL, .after = NULL) {
 
   lhs <- setdiff(seq2(1, where - 1), to_move)
   rhs <- setdiff(seq2(where + 1, ncol(.data)), to_move)
-  new_vars <- vars[unique(c(lhs, to_move, rhs))]
+  new_vars <- .data$vars[unique(c(lhs, to_move, rhs))]
   j <- call2(".", !!!syms(new_vars))
   out <- step_subset_j(.data, vars = new_vars, groups = character(), j = j)
-  step_group(out, groups)
+  step_group(out, .data$groups)
 }
 
 
