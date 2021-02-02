9fb596ef5304bf212bf55f858884dd8e53be2573
Hadley Wickham <h.wickham@gmail.com>
2021-02-02 08:29:50 -0600

    Move select to own file

--- R/step-subset.R
+++ R/step-subset.R
@@ -124,69 +124,6 @@ dt_call.dtplyr_step_subset <- function(x, needs_copy = x$needs_copy) {
 
 # dplyr methods -----------------------------------------------------------
 
-#' Subset columns using their names
-#'
-#' This is a method for the dplyr [select()] generic. It is translated to
-#' the `j` argument of `[.data.table`.
-#'
-#' @param .data A [lazy_dt()].
-#' @inheritParams dplyr::select
-#' @importFrom dplyr select
-#' @export
-#' @examples
-#' library(dplyr, warn.conflicts = FALSE)
-#'
-#' dt <- lazy_dt(data.frame(x1 = 1, x2 = 2, y1 = 3, y2 = 4))
-#'
-#' dt %>% select(starts_with("x"))
-#' dt %>% select(ends_with("2"))
-#' dt %>% select(z1 = x1, z2 = x2)
-select.dtplyr_step <- function(.data, ...) {
-  sim_data <- simulate_vars(.data)
-  locs <- tidyselect::eval_select(expr(c(...)), sim_data)
-  locs <- ensure_group_vars(locs, .data$vars, .data$groups)
-
-  vars <- set_names(.data$vars[locs], names(locs))
-
-  if (length(vars) == 0) {
-    j <- 0L
-    groups <- .data$groups
-  } else {
-    groups <- rename_groups(.data$groups, vars)
-    vars <- simplify_names(vars)
-    j <- call2(".", !!!syms(vars))
-  }
-
-  out <- step_subset_j(.data, vars = names(locs), groups = character(), j = j)
-  step_group(out, groups)
-}
-
-simulate_vars <- function(x, drop_groups = FALSE) {
-  if (drop_groups) {
-    vars <- setdiff(x$vars, x$groups)
-  } else {
-    vars <- x$vars
-  }
-
-  as_tibble(rep_named(vars, list(logical())))
-}
-
-ensure_group_vars <- function(loc, names, groups) {
-  group_loc <- match(groups, names)
-  missing <- setdiff(group_loc, loc)
-
-  if (length(missing) > 0) {
-    vars <- names[missing]
-    inform(paste0(
-      "Adding missing grouping variables: ",
-      paste0("`", names[missing], "`", collapse = ", ")
-    ))
-    loc <- c(set_names(missing, vars), loc)
-  }
-
-  loc
-}
-
 #' Summarise each group to one row
 #'
 #' This is a method for the dplyr [summarise()] generic. It is translated to
@@ -393,17 +330,6 @@ do.dtplyr_step <- function(.data, ...) {
 }
 
 # helpers ------------------------------------------------------------------
-
-rename_groups <- function(groups, vars) {
-  old2new <- set_names(names(vars), vars)
-  groups[groups %in% names(old2new)] <- old2new[groups]
-  groups
-}
-
-simplify_names <- function(vars) {
-  names(vars)[vars == names(vars)] <- ""
-  vars
-}
 
 # For each expression, check if it uses any newly created variables
 check_summarise_vars <- function(dots) {
--- tests/testthat/_snaps/step-subset.md
+++ tests/testthat/_snaps/step-subset.md
@@ -5,10 +5,3 @@
     Error <rlang_error>
       `name` must be a string
 
-# empty select returns no columns
-
-    Code
-      out <- lz %>% group_by(x) %>% select()
-    Message <message>
-      Adding missing grouping variables: `x`
-
