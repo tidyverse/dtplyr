7efcacc0526e8b3eed2f16d2b7d773c3c26aacf1
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2021-02-22 16:52:39 -0700

    Remove unnecessary tests

--- tests/testthat/test-fill.R
+++ tests/testthat/test-fill.R
@@ -1,17 +1,3 @@
-test_that("all missings left unchanged", {
-  tbl <- tibble(
-    int = c(NA_integer_, NA),
-    dbl = c(NA_real_, NA),
-  )
-  dt <- lazy_dt(tbl, "DT")
-
-  down <- fill(dt, int, dbl)
-  up <- fill(dt, int, dbl, .direction = "up")
-
-  expect_identical(collect(down), tbl)
-  expect_identical(collect(up), tbl)
-})
-
 test_that("missings are filled correctly & translations are correct", {
   tbl <- tibble(x = c(NA, 1, NA, 2, NA, NA))
   dt <- lazy_dt(tbl, "DT")
@@ -38,35 +24,4 @@ test_that("auto-conversion to lazy_dt works as intended", {
 
   out <- collect(fill(dt, x))
   expect_equal(out$x, c(NA, 1, 1, 2, 2, 2))
-})
-
-test_that("missings filled down for each atomic vector", {
-  tbl <- tibble(
-    int = c(1L, NA),
-    dbl = c(1, NA)
-  )
-  dt <- lazy_dt(tbl, "DT")
-
-  out <- collect(fill(dt, tidyselect::everything()))
-  expect_equal(out$int, c(1L, 1L))
-  expect_equal(out$dbl, c(1, 1))
-})
-
-test_that("missings filled up for each vector", {
-  tbl <- tibble(
-    int = c(NA, 1L),
-    dbl = c(NA, 1)
-  )
-  dt <- lazy_dt(tbl, "DT")
-
-  out <- collect(fill(dt, tidyselect::everything(), .direction = "up"))
-  expect_equal(out$int, c(1L, 1L))
-  expect_equal(out$dbl, c(1, 1))
-})
-
-test_that("fill respects grouping", {
-  tbl <- tibble(x = c(1, 1, 2), y = c(1, NA, NA))
-  dt <- lazy_dt(tbl, "DT")
-  out <- dt %>% dplyr::group_by(x) %>% fill(y) %>% collect()
-  expect_equal(out$y, c(1, 1, NA))
 })
