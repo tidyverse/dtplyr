92bfdbd6600eefe1f1093ede1ba63bb76f940f42
eutwt <11261404+eutwt@users.noreply.github.com>
2021-08-30 09:40:58 -0400

    simplify expected expr construction

--- tests/testthat/test-tidyeval-across.R
+++ tests/testthat/test-tidyeval-across.R
@@ -63,7 +63,7 @@ test_that("across() captures anonymous functions", {
 
   expect_equal(
    capture_across(dt, across(a, function(x) log(x))),
-   quo_squash(list(a = call2(function(x) log(x), quote(a))))
+   list(a = call2(function(x) log(x), quote(a)))
   )
 })
 
