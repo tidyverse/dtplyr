b550099c0c87419c70a7f1556415c1231b943a23
eutwt <11261404+eutwt@users.noreply.github.com>
2021-09-04 14:43:12 -0400

    Reflect change in documentation

--- R/step-join.R
+++ R/step-join.R
@@ -88,12 +88,11 @@ dt_call.dtplyr_step_join <- function(x, needs_copy = x$needs_copy) {
 #'
 #' These are methods for the dplyr generics [left_join()], [right_join()],
 #' [inner_join()], [full_join()], [anti_join()], and [semi_join()]. Left, right,
-#' inner, and anti join are translated to the `[.data.table` equivalent,
+#' inner, anti and semi join are translated to the `[.data.table` equivalent,
 #' full joins to [data.table::merge.data.table()].
 #' Left, right, and full joins are in some cases followed by calls to
 #' [data.table::setcolorder()] and [data.table::setnames()] to ensure that column
 #' order and names match dplyr conventions.
-#' Semi-joins don't have a direct data.table equivalent.
 #'
 #' @param x,y A pair of [lazy_dt()]s.
 #' @inheritParams dplyr::left_join
