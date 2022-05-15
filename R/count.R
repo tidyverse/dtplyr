99176401acfeda42e4c99ac9fb1a18206f8de657
markfairbanks <mark.t.fairbanks@gmail.com>
2022-05-15 09:47:38 -0600

    Remove `call = caller_env()` from `abort()`

--- R/count.R
+++ R/count.R
@@ -96,7 +96,7 @@ check_name <- function(name, vars) {
       ))
     }
   } else if (!is_string(name)) {
-    abort("`name` must be a string", call = caller_env())
+    abort("`name` must be a string")
   }
 
   name
