cf7c2d8b9ca5586a96fa9ee3e44c9eb56d9d1a33
elephantupthewrongtree <11261404+eutwt@users.noreply.github.com>
2022-07-26 09:52:08 -0400

    Show user-level functions in tidyselect errors. (#379)
    Errors from funs within eval_select use right call

--- R/step-subset-select.R
+++ R/step-subset-select.R
@@ -61,7 +61,12 @@ dtplyr_tidyselect <- function(.data, ...,
     abort("The use of `where()` is not supported by dtplyr.", call = .call)
   }
   sim_data <- simulate_vars(.data, drop_groups = .drop_groups)
-  tidyselect::eval_select(expr(c(!!!dots)), sim_data, allow_rename = .allow_rename)
+  tidyselect::eval_select(
+    expr(c(!!!dots)),
+    sim_data,
+    allow_rename = .allow_rename,
+    error_call = .call
+  )
 }
 
 selection_uses_where <- function(x) {
--- tests/testthat/_snaps/step-call.md
+++ tests/testthat/_snaps/step-call.md
@@ -22,7 +22,7 @@
     Code
       collect(drop_na(dt, "z"))
     Condition
-      Error in `chr_as_locations()`:
+      Error in `drop_na()`:
       ! Can't subset columns that don't exist.
       x Column `z` doesn't exist.
 
